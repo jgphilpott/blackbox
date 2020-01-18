b300b8bba7ac1b0ee47fbbf37869a1587c6a2eb0
Jacob Philpott <jacob.gary.philpott@gmail.com>
2020-01-18 12:38:32 -0500

    adding theme

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 <p align="center">
-  <img width="300" height="300" src="https://github.com/jgphilpott/blackbox/blob/master/app/img/icon.jpg">
+  <img width="200" height="200" src="https://github.com/jgphilpott/blackbox/blob/master/app/img/icon.jpg">
 </p>
 
 # blackbox
--- app/root.py
+++ app/root.py
@@ -1,11 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
+from sass import compile
 
-app = Flask("blackbox")
+app = Flask("blackbox", template_folder="app", static_folder="app")
 
 @app.route("/")
 def home():
-    return "Welcome to blackbox!"
+    return render_template("html/home.html")
 
 if __name__ == "__main__":
+    compile(dirname=("app/sass", "app/css"))
     app.jinja_env.auto_reload = True
     app.run(host="0.0.0.0", port=5000, debug=True)
