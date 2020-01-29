048f0c041187f1d14fc45f4a95d2bcc3801b7ff7
Jacob Philpott <jacob.gary.philpott@gmail.com>
2020-01-29 18:13:18 -0500

    Welcome to the future!

--- app/js/theme.js
+++ app/js/theme.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
 
-  console.log("Welcome to blackbox!")
+  console.log(data)
 
 })
---
+++ app/root.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+from sense_hat import SenseHat
+
+app = Flask("blackbox", template_folder="app", static_folder="app")
+
+sense = SenseHat()
+message = "Welcome to the futrue!"
+sense.show_message(message)
+
+@app.route("/")
+def home():
+    
+    return render_template("html/home.html", message=message)
+
+if __name__ == "__main__":
+    
+    app.jinja_env.auto_reload = True
+    app.run(host="0.0.0.0", port=5000, debug=True)
