7a285af1aba74d25afcde952df77d2103159aff7
Jacob Philpott <jacob.gary.philpott@gmail.com>
2020-01-30 14:39:44 -0500

    sensors update

--- app/sensors/__init__.py
+++ app/sensors/__init__.py
@@ -1,6 +1,19 @@
-from sensors.gyroscope import *
-from sensors.barometer import *
-from sensors.hygrometer import *
-from sensors.thermometer import *
-from sensors.magnetometer import *
-from sensors.accelerometer import *
+from sensors.gyroscope import get_orientation
+from sensors.barometer import get_pressure
+from sensors.hygrometer import get_humidity
+from sensors.thermometer import get_temperature
+from sensors.magnetometer import get_compass
+from sensors.accelerometer import get_acceleration
+
+def get_readings(sense_hat):
+
+	readings = {
+		"orientation": get_orientation(sense_hat),
+		"pressure": get_pressure(sense_hat),
+		"humidity": get_humidity(sense_hat),
+		"temperature": get_temperature(sense_hat),
+		"compass": get_compass(sense_hat),
+		"acceleration": get_acceleration(sense_hat)
+	}
+
+	return readings
