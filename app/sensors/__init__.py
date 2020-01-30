74dd286035d548989e9f5fc592333217309c5c80
Jacob Philpott <jacob.gary.philpott@gmail.com>
2020-01-30 12:07:54 -0500

    adding imports

---
+++ app/sensors/__init__.py
@@ -0,0 +1,6 @@
+from sensors.gyroscope import *
+from sensors.barometer import *
+from sensors.hygrometer import *
+from sensors.thermometer import *
+from sensors.magnetometer import *
+from sensors.accelerometer import *
